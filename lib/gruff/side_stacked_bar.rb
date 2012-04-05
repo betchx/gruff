409c666bf621e007dba443c39a279139ff59ee18
TANABE Atsushi <tanabe.atsushi@nikken.co.jp>
2012-04-05 21:30:50 +0900

    Bug fix: bar shift of SideStackedBar for zero data.
    Shifting bars occured if zero data were given for Gruff::SideStackedBar.

--- lib/gruff/side_stacked_bar.rb
+++ lib/gruff/side_stacked_bar.rb
@@ -3,7 +3,7 @@ require File.dirname(__FILE__) + '/side_bar'
 require File.dirname(__FILE__) + '/stacked_mixin'
 
 ##
-# New gruff graph type added to enable sideways stacking bar charts 
+# New gruff graph type added to enable sideways stacking bar charts
 # (basically looks like a x/y flip of a standard stacking bar chart)
 #
 # alun.eyre@googlemail.com 
@@ -13,7 +13,7 @@ class Gruff::SideStackedBar < Gruff::SideBar
 
   # Spacing factor applied between bars
   attr_accessor :bar_spacing
-  
+
   def draw
     @has_left_labels = true
     get_maximum_by_stack
@@ -37,24 +37,30 @@ class Gruff::SideStackedBar < Gruff::SideBar
     @norm_data.each_with_index do |data_row, row_index|
       data_row[DATA_VALUES_INDEX].each_with_index do |data_point, point_index|
 
-    	  ## using the original calcs from the stacked bar chart to get the difference between
-    	  ## part of the bart chart we wish to stack.
-    	  temp1 = @graph_left + (@graph_width -
-                                    data_point * @graph_width - 
-                                    height[point_index]) + 1
-    	  temp2 = @graph_left + @graph_width - height[point_index] - 1
-    	  difference = temp2 - temp1
+        ## using the original calcs from the stacked bar chart to get the difference between
+        ## part of the bart chart we wish to stack.
+        temp1 = @graph_left + (@graph_width -
+                               data_point * @graph_width -
+                               height[point_index]) + 1
+        temp2 = @graph_left + @graph_width - height[point_index] - 1
+        difference = temp2 - temp1
 
-    	  @d = @d.fill data_row[DATA_COLOR_INDEX]
+        @d = @d.fill data_row[DATA_COLOR_INDEX]
 
-        left_x = length[point_index] #+ 1
-              left_y = @graph_top + (@bar_width * point_index) + padding
-    	  right_x = left_x + difference
-              right_y = left_y + @bar_width * @bar_spacing
-    	  length[point_index] += difference
+        # In order to avoid shifting bars to left
+        # when data_point was close to zero, rectangle
+        # was drawn only if differcence was greather
+        # than zero.
+        if difference > 0
+          left_x = length[point_index] #+ 1
+          left_y = @graph_top + (@bar_width * point_index) + padding
+          right_x = left_x + difference
+          right_y = left_y + @bar_width * @bar_spacing
+          length[point_index] += difference
+
+          @d = @d.rectangle(left_x, left_y, right_x, right_y)
+        end
         height[point_index] += (data_point * @graph_width - 2)
-
-        @d = @d.rectangle(left_x, left_y, right_x, right_y)
 
         # Calculate center based on bar_width and current row
         label_center = @graph_top + (@bar_width * point_index) + (@bar_width * @bar_spacing / 2.0)
@@ -63,7 +69,7 @@ class Gruff::SideStackedBar < Gruff::SideBar
 
     end
 
-    @d.draw(@base_image)    
+    @d.draw(@base_image)
   end
 
   def larger_than_max?(data_point, index=0)
